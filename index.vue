dc5b0d744efe005a716b37d54f243c356af8b3be
Kamuhikari <91415443+kamuhikari@users.noreply.github.com>
2025-04-25 16:15:21 +0700

    intial commit

---
+++ index.vue
@@ -0,0 +1,29 @@
+<script>
+import textContent from './text.json';
+
+export default {
+  data() {
+    return {
+      content: textContent  // Load all text externally
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ content.greeting }}</h1>
+    <p>{{ content.wish }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+    color: blue;
+}
+
+p {
+    font-size: 16px;
+    color: gray;
+}
+</style>
